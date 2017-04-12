3309c414e9bd53b7292992fd9e64a707e8ca768a
Adam <a.stankiewicz7@gmail.com>
2017-04-12 12:39:14 +0200

    Copleted desktop and mobile header styles.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+ const path = require('path');
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+  
+   path: path.resolve(__dirname, './app/temp/scripts'),
+   filename: "App.js"
+  
+  }, 
+
+  module: {
+  	loaders: [
+      {
+      	loader: 'babel',
+        include: [
+        path.resolve(__dirname, "./app/temp/scripts")
+         ],
+
+      	query: {
+      		presets: ['es2015']
+      	},
+      	test: /\.js$/,
+      	exclude: ['node_modules'] 
+      }
+  	]
+  }
+};
